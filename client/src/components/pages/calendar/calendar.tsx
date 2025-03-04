33c71222543ebe820b16b93bd5d878abd7207dd1
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2025-03-04 13:20:40 +0300

    Предварительная версия режима мастера

--- client/src/components/pages/calendar/calendar.tsx
+++ client/src/components/pages/calendar/calendar.tsx
@@ -1,4 +1,4 @@
-import { h } from "preact"; // eslint-disable-line
+import { h } from "preact";
 
 import "@schedule-x/theme-default/dist/index.css";
 import "./calendar.css";
@@ -13,6 +13,7 @@ import { route as navigate } from "preact-router";
 import { useStore } from "@nanostores/preact";
 
 import {
+	check,
 	createEvent,
 	IApiCompany,
 	IApiLocation,
@@ -22,6 +23,8 @@ import {
 	readMyCompanies,
 } from "../../../api";
 import { $tz } from "../../../store/tz";
+import { $mastery } from "../../../store/mastery";
+import { enableMastery, disableMastery } from "../../../store/mastery";
 import {
 	Button,
 	Container,
@@ -32,6 +35,7 @@ import {
 	InputAddon,
 	NativeSelect,
 	Stack,
+	Switch,
 } from "@chakra-ui/react";
 
 import {
@@ -51,6 +55,8 @@ import { Company } from "./company";
 import { Location } from "./location";
 import type { UUID } from "node:crypto";
 import toast from "react-hot-toast";
+import { CalendarAppSingleton } from "@schedule-x/shared";
+import { MASTERY_KEY, TRUE } from "../../../store/mastery";
 
 const EVENT_FORMAT = "YYYY-MM-DD HH:mm";
 const DEFAULT_EVENT_DURATION = 4;
@@ -61,31 +67,40 @@ interface IFormCreateEvent {
 	readonly location: UUID;
 	readonly start: string;
 	readonly startTime: string;
-	readonly max_slots: string,
-	readonly plan_duration: string,
+	readonly max_slots: string;
+	readonly plan_duration: string;
 }
 
 export const CalendarPage = () => {
 	const [openDraw, setOpenDraw] = useState(false);
-	const [isDisableCreateEventButton, setIsDisableCreateEventButton] = useState(false);
+	const [isDisableCreateEventButton, setIsDisableCreateEventButton] =
+		useState(false);
 	const [companyList, setCompanyList] = useState<IApiCompany[]>([]);
 	const [locationList, setLocationList] = useState<IApiLocation[]>([]);
 
 	const tz = useStore($tz);
-	const { register, handleSubmit, reset } =
-		useForm<IFormCreateEvent>();
+	const mastery = useStore($mastery);
 
-	const addDataEventToCalendar = (dateStart: string, dateEnd: string, calendar: CalendarApp) => {
+	const { register, handleSubmit, reset } = useForm<IFormCreateEvent>();
+
+	const addDataEventToCalendar = (
+		dateStart: string,
+		dateEnd: string,
+		calendar: CalendarApp,
+	) => {
+		console.log(mastery);
 		const dateStartWithTz = dayjs(dateStart).tz(tz, KEEP_LOCAL_TIME).format();
 		const dateEndWithTz = dayjs(dateEnd).tz(tz, KEEP_LOCAL_TIME).format();
-		readEventsList(dateStartWithTz, dateEndWithTz).then((res) => {
+		readEventsList(dateStartWithTz, dateEndWithTz, {
+			imamaster: localStorage.getItem(MASTERY_KEY) === TRUE,
+		}).then((res) => {
 			if (res !== null) {
 				calendar.events.set(
 					res.payload.map((apiEv) => {
 						const start = dayjs(apiEv.date);
 						const end = start.add(
 							apiEv.plan_duration || DEFAULT_EVENT_DURATION,
-							"h"
+							"h",
 						);
 
 						return {
@@ -96,11 +111,11 @@ export const CalendarPage = () => {
 							start: start.format(EVENT_FORMAT),
 							end: end.format(EVENT_FORMAT),
 						};
-					})
+					}),
 				);
 			}
 		});
-	}
+	};
 
 	const calendar = useCalendarApp({
 		locale: "ru-RU",
@@ -114,12 +129,21 @@ export const CalendarPage = () => {
 			},
 			onRender(app) {
 				const range = app.calendarState.range.value;
-				if(range !== null) {
+				if (range !== null) {
 					addDataEventToCalendar(range.start, range.end, calendar);
 				}
-			}
+			},
 		},
 	});
+
+	useEffect(() => {
+		const app = calendar["$app"] as CalendarAppSingleton;
+		const range = app.calendarState.range.value;
+		if (range === null) {
+			return;
+		}
+		addDataEventToCalendar(range.start, range.end, calendar);
+	}, [mastery]);
 
 	const getCompanies = () => {
 		return readMyCompanies().then((responce) => {
@@ -146,7 +170,7 @@ export const CalendarPage = () => {
 				const start = dayjs(data.date);
 				const end = start.add(
 					data.plan_duration || DEFAULT_EVENT_DURATION,
-					"h"
+					"h",
 				);
 
 				calendar.events.add({
@@ -185,7 +209,9 @@ export const CalendarPage = () => {
 	}, []);
 
 	function handleKeyDown(event: KeyboardEvent) {
-		if (event.key === "Escape") { setOpenDraw(false); }
+		if (event.key === "Escape") {
+			setOpenDraw(false);
+		}
 	}
 
 	const onSubmit = handleSubmit((data) => {
@@ -200,7 +226,7 @@ export const CalendarPage = () => {
 				date.toISOString(),
 				location,
 				Number(max_slots) || null,
-				Number(plan_duration) || null
+				Number(plan_duration) || null,
 			).then((res) => {
 				if (res) {
 					toast.success("Событие успешно создано");
@@ -215,131 +241,165 @@ export const CalendarPage = () => {
 	return (
 		<section>
 			<Container>
-				<Stack mb={4} direction="row" gap={4}>
-					<DrawerRoot
-						open={openDraw}
-						onOpenChange={(e) => {
-							if (e.open) {
-								setIsDisableCreateEventButton(true);
-								Promise.all([getCompanies(), getLocations()]).then(([companiesResult, locationsResult]) => {
-									if(companiesResult === null || locationsResult === null) {
-										setIsDisableCreateEventButton(false);
-										return;
-									}
-									setOpenDraw(e.open)
-									setIsDisableCreateEventButton(false);
-								});
-							} else {
-								setOpenDraw(e.open);
-							}
-						}}
-					>
-						<DrawerBackdrop />
-						<DrawerTrigger asChild>
-							<Button disabled={isDisableCreateEventButton} variant="outline">Добавить событие</Button>
-						</DrawerTrigger>
-						<DrawerContent>
-							<DrawerHeader>
-								<DrawerTitle>Создание события</DrawerTitle>
-							</DrawerHeader>
-							<DrawerBody>
-								<form onSubmit={onSubmit}>
-									<Stack gap="4" w="full">
-										<Field label="Кампания">
-											<NativeSelect.Root>
-												<NativeSelect.Field
-													placeholder="Выберите из списка"
-													{...register("company", {
-														required: "Заполните",
-													})}
-													defaultValue={companyList?.[0]?.id}
-												>
-													{companies.items.map((company) => (
-														<option
-															value={company.id}
-															key={company.name}
-														>
-															{company.name}
-														</option>
-													))}
-												</NativeSelect.Field>
-												<NativeSelect.Indicator />
-											</NativeSelect.Root>
-										</Field>
-										<HStack gap={2} width="full">
-											<Field label="Начало">
-												<Input
-													type="date"
-													{...register("start", {
-														required: "Заполните поле",
-													})}
-												/>
+				<Switch.Root
+					mb={5}
+					size="lg"
+					checked={mastery}
+					onCheckedChange={() => {
+						if (mastery) {
+							disableMastery();
+						} else {
+							check(true).then((isAuth) => {
+								if (isAuth) {
+									enableMastery();
+								}
+							});
+						}
+					}}
+				>
+					<Switch.HiddenInput />
+					<Switch.Control>
+						<Switch.Thumb />
+					</Switch.Control>
+					<Switch.Label>Режим мастера</Switch.Label>
+				</Switch.Root>
+				{mastery && (
+					<Stack mb={4} direction="row" gap={4}>
+						<DrawerRoot
+							open={openDraw}
+							onOpenChange={(e) => {
+								if (e.open) {
+									setIsDisableCreateEventButton(true);
+									Promise.all([getCompanies(), getLocations()]).then(
+										([companiesResult, locationsResult]) => {
+											if (
+												companiesResult === null ||
+												locationsResult === null
+											) {
+												setIsDisableCreateEventButton(false);
+												return;
+											}
+											setOpenDraw(e.open);
+											setIsDisableCreateEventButton(false);
+										},
+									);
+								} else {
+									setOpenDraw(e.open);
+								}
+							}}
+						>
+							<DrawerBackdrop />
+							<DrawerTrigger asChild>
+								<Button
+									disabled={isDisableCreateEventButton}
+									variant="outline"
+								>
+									Добавить событие
+								</Button>
+							</DrawerTrigger>
+							<DrawerContent>
+								<DrawerHeader>
+									<DrawerTitle>Создание события</DrawerTitle>
+								</DrawerHeader>
+								<DrawerBody>
+									<form onSubmit={onSubmit}>
+										<Stack gap="4" w="full">
+											<Field label="Кампания">
+												<NativeSelect.Root>
+													<NativeSelect.Field
+														placeholder="Выберите из списка"
+														{...register("company", {
+															required: "Заполните",
+														})}
+														defaultValue={companyList?.[0]?.id}
+													>
+														{companies.items.map((company) => (
+															<option
+																value={company.id}
+																key={company.name}
+															>
+																{company.name}
+															</option>
+														))}
+													</NativeSelect.Field>
+													<NativeSelect.Indicator />
+												</NativeSelect.Root>
 											</Field>
-											<Field label="Время">
-												<Input
-													type="time"
-													{...register("startTime", {
-														required: "Заполните поле",
-													})}
-												/>
+											<HStack gap={2} width="full">
+												<Field label="Начало">
+													<Input
+														type="date"
+														{...register("start", {
+															required: "Заполните поле",
+														})}
+													/>
+												</Field>
+												<Field label="Время">
+													<Input
+														type="time"
+														{...register("startTime", {
+															required: "Заполните поле",
+														})}
+													/>
+												</Field>
+											</HStack>
+											<Field label="Локация">
+												<NativeSelect.Root>
+													<NativeSelect.Field
+														placeholder="Выберите из списка"
+														{...register("location", {
+															required: "Заполните",
+														})}
+														defaultValue={locationList?.[0]?.id}
+													>
+														{locations.items.map((location) => (
+															<option
+																value={location.id}
+																key={location.name}
+															>
+																{location.name}
+															</option>
+														))}
+													</NativeSelect.Field>
+													<NativeSelect.Indicator />
+												</NativeSelect.Root>
 											</Field>
-										</HStack>
-										<Field label="Локация">
-											<NativeSelect.Root>
-												<NativeSelect.Field
-													placeholder="Выберите из списка"
-													{...register("location", {
-														required: "Заполните",
-													})}
-													defaultValue={locationList?.[0]?.id}
-												>
-													{locations.items.map((location) => (
-														<option
-															value={location.id}
-															key={location.name}
-														>
-															{location.name}
-														</option>
-													))}
-												</NativeSelect.Field>
-												<NativeSelect.Indicator />
-											</NativeSelect.Root>
-										</Field>
 
-										<Field label="Максимальное количество игроков">
-											<Input
-												type="number"
-												min="1"
-												step="1"
-												defaultValue="1"
-												{...register("max_slots")}
-											/>
-										</Field>
-
-										<Field label="Планируемая длительность">
-											<Group attached w="full">
+											<Field label="Максимальное количество игроков">
 												<Input
 													type="number"
 													min="1"
 													step="1"
 													defaultValue="1"
-													{...register("plan_duration")}
+													{...register("max_slots")}
 												/>
-												<InputAddon>час</InputAddon>
-											</Group>
-										</Field>
-									</Stack>
-									<Button type="submit" w="full" mt={6}>
-										Создать
-									</Button>
-								</form>
-							</DrawerBody>
-							<DrawerCloseTrigger />
-						</DrawerContent>
-					</DrawerRoot>
-					<Company data={companyList} />
-					<Location />
-				</Stack>
+											</Field>
+
+											<Field label="Планируемая длительность">
+												<Group attached w="full">
+													<Input
+														type="number"
+														min="1"
+														step="1"
+														defaultValue="1"
+														{...register("plan_duration")}
+													/>
+													<InputAddon>час</InputAddon>
+												</Group>
+											</Field>
+										</Stack>
+										<Button type="submit" w="full" mt={6}>
+											Создать
+										</Button>
+									</form>
+								</DrawerBody>
+								<DrawerCloseTrigger />
+							</DrawerContent>
+						</DrawerRoot>
+						<Company data={companyList} />
+						<Location />
+					</Stack>
+				)}
 				<ScheduleXCalendar calendarApp={calendar} />
 			</Container>
 		</section>
--- client/src/store/mastery.ts
+++ client/src/store/mastery.ts
@@ -1,16 +1,18 @@
-import { atom, ReadableAtom, WritableAtom } from 'nanostores';
+import { atom, ReadableAtom, WritableAtom } from "nanostores";
 
-const MASTERY_KEY = 'nri_mastery';
-const TRUE = 'true'
+export const MASTERY_KEY = "nri_mastery";
+export const TRUE = "true";
 
-export const $mastery: ReadableAtom<boolean> = atom(localStorage.getItem(MASTERY_KEY) === TRUE);
+export const $mastery: ReadableAtom<boolean> = atom(
+	localStorage.getItem(MASTERY_KEY) === TRUE,
+);
 
 export const enableMastery = () => {
 	localStorage.setItem(MASTERY_KEY, TRUE);
 	($mastery as WritableAtom<boolean>).set(true);
-}
+};
 
 export const disableMastery = () => {
 	localStorage.removeItem(MASTERY_KEY);
 	($mastery as WritableAtom<boolean>).set(false);
-}
+};
