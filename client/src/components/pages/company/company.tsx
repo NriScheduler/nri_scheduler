a08f2c1ca16d18fbf50a8a6e1ec562f76ca8fd27
Владислав Дворянинов <v.dvoryaninov@notamedia.ru>
2025-03-07 09:39:09 +0300

    Убрал ругать на не правильно вложенные теги

--- client/src/components/pages/company/company.tsx
+++ client/src/components/pages/company/company.tsx
@@ -47,26 +47,24 @@ const CompanyCard = ({ company }: { company: IApiCompanyInfo }) => {
 				<HStack mb="6" gap="3">
 					<Heading size="3xl">Компания - {company.name}</Heading>
 				</HStack>
-				<Card.Description>
-					<DataList.Root orientation="horizontal">
-						{stats.map((item) => (
-							<DataList.Item key={item.label}>
-								<DataList.ItemLabel minW="150px">
-									{item.label}
-								</DataList.ItemLabel>
-								<DataList.ItemValue color="black" fontWeight="500">
-									{item.href ? (
-										<Link href={item.href} colorPalette="blue">
-											{item.value}
-										</Link>
-									) : (
-										<span>{item.value}</span>
-									)}
-								</DataList.ItemValue>
-							</DataList.Item>
-						))}
-					</DataList.Root>
-				</Card.Description>
+				<DataList.Root orientation="horizontal">
+					{stats.map((item) => (
+						<DataList.Item key={item.label}>
+							<DataList.ItemLabel minW="150px">
+								{item.label}
+							</DataList.ItemLabel>
+							<DataList.ItemValue color="black" fontWeight="500">
+								{item.href ? (
+									<Link href={item.href} colorPalette="blue">
+										{item.value}
+									</Link>
+								) : (
+									<span>{item.value}</span>
+								)}
+							</DataList.ItemValue>
+						</DataList.Item>
+					))}
+				</DataList.Root>
 			</Card.Body>
 			<Card.Footer></Card.Footer>
 		</Card.Root>
@@ -87,20 +85,18 @@ const CompanyCardSkeleton = () => {
 				<HStack mb="6" gap="3">
 					<Skeleton height="38px" w="30%" />
 				</HStack>
-				<Card.Description>
-					<DataList.Root orientation="horizontal">
-						{stats.map((item, index) => (
-							<DataList.Item key={index}>
-								<DataList.ItemLabel minW="150px">
-									{item.label}
-								</DataList.ItemLabel>
-								<DataList.ItemValue color="black" fontWeight="500">
-									<Skeleton height="20px" w="30%" />
-								</DataList.ItemValue>
-							</DataList.Item>
-						))}
-					</DataList.Root>
-				</Card.Description>
+				<DataList.Root orientation="horizontal">
+					{stats.map((item, index) => (
+						<DataList.Item key={index}>
+							<DataList.ItemLabel minW="150px">
+								{item.label}
+							</DataList.ItemLabel>
+							<DataList.ItemValue color="black" fontWeight="500">
+								<Skeleton height="20px" w="30%" />
+							</DataList.ItemValue>
+						</DataList.Item>
+					))}
+				</DataList.Root>
 			</Card.Body>
 		</Card.Root>
 	);
